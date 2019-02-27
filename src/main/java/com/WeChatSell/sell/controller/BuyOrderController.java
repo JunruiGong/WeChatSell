0ae08388f5d2e59e30c05d3117123b3d6a6da7aa
Junrui Gong <jgong8@hawk.iit.edu>
2019-02-27 10:59:42 -0600

    订单详情/取消订单

--- src/main/java/com/WeChatSell/sell/controller/BuyOrderController.java
+++ src/main/java/com/WeChatSell/sell/controller/BuyOrderController.java
@@ -11,14 +11,16 @@ import com.WeChatSell.sell.service.OrderService;
 import com.WeChatSell.sell.util.ResultVOUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -40,22 +42,59 @@ public class BuyOrderController {
 
         OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
 
-        if(CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())) {
             log.error("【创建订单】购物车不能为空");
             throw new SellException(ResultEnum.CART_EMPTY);
         }
 
         OrderDTO createResult = orderService.createOrder(orderDTO);
 
-        Map<String,String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
 
         map.put("orderId", createResult.getOrderId());
         return ResultVOUtil.success(map);
     }
 
     // 订单列表
+    @GetMapping("/list")
+    public ResultVO<List<OrderDTO>> list(@RequestParam("openid") String openid,
+                                         @RequestParam(value="page", defaultValue = "0") Integer page,
+                                         @RequestParam(value = "size", defaultValue = "10") Integer size) {
+
+        if (StringUtils.isEmpty(openid)){
+            log.error("【查询订单列表】openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+
+        PageRequest pageRequest = new PageRequest(page,size);
+        Page<OrderDTO> orderDTOPage = orderService.findList(openid, pageRequest);
+
+        return ResultVOUtil.success(orderDTOPage.getContent());
+
+    }
+
+    // 订单详情
+    @GetMapping("/detail")
+    public ResultVO<OrderDTO> detail(@RequestParam("openid") String openid,
+                                     @RequestParam("orderId") String orderId){
+
+        //TODO 安全验证
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        return ResultVOUtil.success(orderDTO);
+
+    }
 
     // 取消订单
+    @PostMapping("/cancel")
+    public ResultVO cancel(@RequestParam("openid") String openid,
+                           @RequestParam("orderId") String orderId){
 
-    // 取消订单
+        //TODO 安全验证
+
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        orderService.cancel(orderDTO);
+
+        return ResultVOUtil.success();
+    }
+
 }
--- src/main/java/com/WeChatSell/sell/dto/OrderDTO.java
+++ src/main/java/com/WeChatSell/sell/dto/OrderDTO.java
@@ -3,6 +3,9 @@ package com.WeChatSell.sell.dto;
 import com.WeChatSell.sell.dataobject.OrderDetail;
 import com.WeChatSell.sell.enums.OrderStatusEnum;
 import com.WeChatSell.sell.enums.PayStatusEnum;
+import com.WeChatSell.sell.util.serializer.Date2LongSerializer;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 
 import javax.persistence.Id;
@@ -11,6 +14,7 @@ import java.util.Date;
 import java.util.List;
 
 @Data
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
 
     private String orderId;
@@ -31,8 +35,10 @@ public class OrderDTO {
     // 支付状态，默认为0，未支付
     private Integer payStatus;
 
+    @JsonSerialize(using= Date2LongSerializer.class)
     private Date createTime;
 
+    @JsonSerialize(using= Date2LongSerializer.class)
     private Date updateTime;
 
     private List<OrderDetail> orderDetailList;
